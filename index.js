9dd77a369d22f2e879dd2eb6fc44e3daa2994267
Ростислав Шведов <rustysquezzy@outlook.com>
2026-04-27 22:06:57 +0300

    fix: add empty input guard, edit undo and accept (with keyboard) and storage updating, also fix code formatting

--- index.js
+++ index.js
@@ -1,91 +1,105 @@
-let items = [
-	"Сделать проектную работу",
-	"Полить цветы",
-	"Пройти туториал по Реакту",
-	"Сделать фронт для своего проекта",
-	"Прогуляться по улице в солнечный день",
-	"Помыть посуду",
-];
-
-const listElement = document.querySelector(".to-do__list");
-const formElement = document.querySelector(".to-do__form");
-const inputElement = document.querySelector(".to-do__input");
-
+const listElement = document.querySelector('.to-do__list');
+const formElement = document.querySelector('.to-do__form');
+const inputElement = document.querySelector('.to-do__input');
 
 formElement.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const taskText = inputElement.value;
-	
-	const todo = createItem(taskText);
-	listElement.prepend(todo);
-	
-	items = getTasksFromDOM();
-	saveTasks(items);
-	inputElement.value = '';
+  event.preventDefault();
+
+  const taskText = inputElement.value;
+  if (taskText.trim() === '') {
+    return;
+  }
+
+  const todo = createItem(taskText);
+  listElement.prepend(todo);
+
+  updateStorage()
+  inputElement.value = '';
 });
 
 function loadTasks() {
-	if (localStorage.getItem('tasks')) {
-		return JSON.parse(localStorage.getItem('tasks'));
-	}
-	return items;
+  if (localStorage.getItem('tasks')) {
+    return JSON.parse(localStorage.getItem('tasks'));
+  }
+  return ['Сделать проектную работу',
+					'Полить цветы',
+  				'Пройти туториал по Реакту',
+  				'Сделать фронт для своего проекта',
+  				'Прогуляться по улице в солнечный день',
+  				'Помыть посуду',
+	];
 }
 
 function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
-	
-	textElement.textContent = item;
+  const template = document.getElementById('to-do__item-template');
+  const clone = template.content.querySelector('.to-do__item').cloneNode(true);
+  const textElement = clone.querySelector('.to-do__item-text');
+  const deleteButton = clone.querySelector('.to-do__item-button_type_delete');
+  const duplicateButton = clone.querySelector(
+    '.to-do__item-button_type_duplicate',
+  );
+  const editButton = clone.querySelector('.to-do__item-button_type_edit');
 
-	deleteButton.addEventListener('click', () => {
-		clone.remove();
-		const items = getTasksFromDOM();
-		saveTasks(items); 
-	});
+  textElement.textContent = item;
 
-	duplicateButton.addEventListener('click', () => {
-		const itemName = textElement.textContent;
-		const newItem = createItem(itemName);
-		listElement.prepend(newItem);
+  deleteButton.addEventListener('click', () => {
+    clone.remove();
+    updateStorage();
+  });
 
-		const items = getTasksFromDOM();
-		saveTasks(items);
-	});
+  duplicateButton.addEventListener('click', () => {
+    const itemName = textElement.textContent;
+    const newItem = createItem(itemName);
+    listElement.prepend(newItem);
+    updateStorage();
+  });
 
-	editButton.addEventListener('click', event => {
-	  textElement.setAttribute('contenteditable', 'true');
-		textElement.focus();
-	});
+
+	let originalText = textElement.textContent;
+
+	editButton.addEventListener('click', () => {
+  	originalText = textElement.textContent;
+    textElement.setAttribute('contenteditable', 'true');
+    textElement.focus();
+  });
+
+  textElement.addEventListener('keydown', event => {
+    if (event.code === 'Enter') {
+      textElement.removeAttribute('contenteditable');
+    } else if (event.code === 'Escape') {
+      textElement.textContent = originalText;
+      textElement.removeAttribute('contenteditable');
+    }
+    updateStorage();
+  });
 
   textElement.addEventListener('blur', () => {
-		textElement.setAttribute('contenteditable', 'false');
-		const items = getTasksFromDOM();
-		saveTasks(items);
-	});
+		textElement.removeAttribute('contenteditable');
+    updateStorage();
+  });
 
-	return clone;
+  return clone;
 }
 
 function getTasksFromDOM() {
-	const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
-	const tasks = [];
-	itemsNamesElements.forEach(itemName => tasks.push(itemName.textContent));
+  const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
+  const tasks = [];
+  itemsNamesElements.forEach((itemName) => tasks.push(itemName.textContent));
 
-	return tasks;
+  return tasks;
 }
 
 function saveTasks(tasks) {
-	const tasksForStoring = JSON.stringify(tasks);
-	localStorage.setItem('tasks', tasksForStoring);
+  const tasksForStoring = JSON.stringify(tasks);
+  localStorage.setItem('tasks', tasksForStoring);
 }
 
-items = loadTasks();
-items.forEach((task) => {
-	const todo = createItem(task);
-	listElement.append(todo)
-});
+function updateStorage() {
+	saveTasks(getTasksFromDOM());
+}
 
+const items = loadTasks();
+items.forEach((task) => {
+  const todo = createItem(task);
+  listElement.append(todo);
+});
